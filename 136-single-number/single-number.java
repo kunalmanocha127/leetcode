b178f4289363806e4205a8fb2076abba219e8481
kunalmanocha127 <kunalmanocha.127@gmail.com>
2026-05-01 23:08:25 +0530

    Time: 1 ms (99.97%) | Memory: 46.8 MB (64.77%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int op = 0;
+        for (int i = 0; i < nums.length; i++) {
+            op = op ^ nums[i];
+        }
+        return op;
+    }
+}
